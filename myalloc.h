614e96ff4fac7039eddff40bc402d77e04c9c1cb
Lucas <55973521+lucasrouchy@users.noreply.github.com>
2022-02-21 15:49:31 -0800

    added the struct block to myalloc.h

---
+++ myalloc.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+
+#define ALIGNMENT 16   // Must be power of 2
+#define GET_PAD(x) ((ALIGNMENT - 1) - (((x) - 1) & (ALIGNMENT - 1)))
+
+#define PADDED_SIZE(x) ((x) + GET_PAD(x))
+#define PTR_OFFSET(p, offset) ((void*)((char *)(p) + (offset)))
+
+struct block {
+  struct block *next;
+  int size;
+  int in_use;
+};
